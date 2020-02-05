c9ee4e9f6ca8a78db819cbf2b122f875902a9b73
TheoMoriceEM <theo.morice5@gmail.com>
2020-02-05 15:19:20 +0100

    Create migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->enum('status', ['admin', 'client']);
+            $table->unsignedDecimal('balance')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_02_05_140911_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('fk_currency');
+            $table->foreign('fk_currency')->references('id')->on('currencies');
+            $table->unsignedBigInteger('fk_user');
+            $table->foreign('fk_user')->references('id')->on('users');
+            $table->unsignedDecimal('amount');
+            $table->unsignedInteger('quantity');
+            $table->unsignedDecimal('purchase_price');
+            $table->timestamp('purchase_date');
+            $table->boolean('sold');
+            $table->unsignedDecimal('selling_amount')->nullable();
+            $table->timestamp('selling_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
