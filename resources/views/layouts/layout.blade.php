af573a1498cbd21d7cf6a0a1b5c432c741f91304
TheoMoriceEM <theo.morice5@gmail.com>
2020-02-06 15:33:47 +0100

    Add FontAwesome library

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://kit.fontawesome.com/badf7f3a33.js" crossorigin="anonymous"></script>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
