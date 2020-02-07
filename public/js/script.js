57f34308445d3baffa0be6d1d202f0da0a0a95e0
TheoMoriceEM <theo.morice5@gmail.com>
2020-02-07 18:55:18 +0100

    Refactor JS in jQuery

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,8 @@
 /**
  * Manage mobile nav
  */
-const closeNav = document.querySelector("#closeNav");
-const toggleNav = document.querySelector("#toggleNav");
-const sidebar = document.querySelector("#sidebar");
-
-closeNav.addEventListener('click', function() {
-    sidebar.classList.add('hidden');
-});
-
-toggleNav.addEventListener('click', function() {
-    sidebar.classList.remove('hidden');
+$(function () {
+    $("#closeNav, #toggleNav").click(function() {
+        $("#sidebar").toggleClass('hidden');
+    });
 });
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -82,6 +82,7 @@
         </main>
 
         {{-- Scripts --}}
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="{{ asset('js/script.js') }}"></script>
         @yield('JS')
     </body>
