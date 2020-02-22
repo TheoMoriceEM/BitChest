aeafec44054d7138d1deb23ca69fd482b1b4369a
TheoMoriceEM <theo.morice5@gmail.com>
2020-02-22 22:40:57 +0100

    Currencies index view back

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use App\Currency;
 
 class CurrencyController extends Controller
 {
@@ -11,7 +13,28 @@ class CurrencyController extends Controller
      */
     public function index()
     {
-        return view('currencies.index');
+        $currencies = Currency::all(); // Get all the currencies
+
+        // Get their current rates through an API
+        foreach ($currencies as $currency) {
+            $client = new Client([
+                'base_uri' => 'https://min-api.cryptocompare.com'
+            ]);
+
+            // Request to the API for getting the current rate
+            $response = $client->get('data/price', [
+                'query' => [
+                    'fsym'  => $currency->api_id, // Currency's identifier
+                    'tsyms' => config('currency')['api_id'] // Real currency used for conversion
+                ]
+            ]);
+
+            // Get the rate from JSON response
+            $conversion_currency = config('currency')['api_id'];
+            $currency->current_rate = json_decode($response->getBody())->$conversion_currency;
+        }
+
+        return view('currencies.index', ['currencies' => $currencies]);
     }
 
     /**
--- resources/views/currencies/index.blade.php
+++ resources/views/currencies/index.blade.php
@@ -14,24 +14,19 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Bitcoin</td>
-                <td>BTC</td>
-                <td class="d-flex align-items-center">3056,87 €<i class="fas fa-2x fa-caret-up ml-2" style="color: #58ee58"></i></td>
-                <td><a class="btn btn-sm btn-outline-secondary" href="#" role="button">Historique</a></td>
-                <td><a class="btn btn-sm btn-primary" href="#" role="button">Acheter</a></td>
-            </tr>
+            @foreach ($currencies as $currency)
+                <tr>
+                    <td>{{ $currency->name }}</td>
+                    <td>{{ $currency->api_id }}</td>
+                    <td class="d-flex align-items-center">{{ $currency->current_rate }} {{ config('currency')['symbol'] }}<i class="fas fa-2x fa-caret-up ml-2" style="color: #58ee58"></i></td>
+                    <td><a class="btn btn-sm btn-outline-secondary" href="#" role="button">Historique</a></td>
+                    <td><a class="btn btn-sm btn-primary" href="#" role="button">Acheter</a></td>
+                </tr>
+            @endforeach
             <tr>
                 <td>Ethereum</td>
                 <td>ETH</td>
                 <td class="d-flex align-items-center">826,24 €<i class="fas fa-2x fa-caret-down ml-2" style="color: #ff3c3c"></i></td>
-                <td><a class="btn btn-sm btn-outline-secondary" href="#" role="button">Historique</a></td>
-                <td><a class="btn btn-sm btn-primary" href="#" role="button">Acheter</a></td>
-            </tr>
-            <tr>
-                <td>Ripple</td>
-                <td>XRP</td>
-                <td class="d-flex align-items-center">214,07 €<i class="fas fa-2x fa-caret-up ml-2" style="color: #58ee58"></i></td>
                 <td><a class="btn btn-sm btn-outline-secondary" href="#" role="button">Historique</a></td>
                 <td><a class="btn btn-sm btn-primary" href="#" role="button">Acheter</a></td>
             </tr>
