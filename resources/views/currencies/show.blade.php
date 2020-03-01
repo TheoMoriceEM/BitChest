bb26045c07144106686c203b0fc3e0b828691ced
TheoMoriceEM <theo.morice5@gmail.com>
2020-03-01 16:15:25 +0100

    Currencies show front

--- resources/views/currencies/show.blade.php
+++ resources/views/currencies/show.blade.php
@@ -3,22 +3,42 @@
 @section('title', 'Cours du Bitcoin')
 
 @section('content')
-    <h1 class="text-center mb-5">Historique du Bitcoin</h1>
-    <div class="d-flex justify-content-center">
+    <h1 class="text-center">Historique du Bitcoin</h1>
+    <div class="d-flex justify-content-center my-4">
         <a class="btn btn-sm btn-outline-secondary" href="{{ route('currencies.index') }}" role="button">Retour</a>
         <a class="btn btn-sm btn-primary ml-3" href="#" role="button">Acheter</a>
     </div>
-    <canvas id="historyChart" width="400" height="400"></canvas>
+    <canvas id="historyChart"></canvas>
 @endsection
 
 @section('JS')
     <script>
-        const ctx = $('#historyChart');
-
-        const chart = new Chart(ctx, {
+        new Chart($('#historyChart'), {
             type: 'line',
-            data: {},
-            options: {}
+            data: {
+                labels: [
+                    '01/02', '', '', '', '',
+                    '06/02', '', '', '', '',
+                    '11/02', '', '', '', '',
+                    '16/02', '', '', '', '',
+                    '21/02', '', '', '', '',
+                    '26/02', '', '', '', '01/03'
+                ],
+                datasets: [{
+                    label: 'Cours du Bitcoin',
+                    data: [
+                        65, 59, 80, 81, 56,
+                        55, 40, 28, 36, 64,
+                        65, 59, 80, 81, 56,
+                        55, 40, 28, 36, 64,
+                        65, 59, 80, 81, 56,
+                        55, 40, 28, 36, 64
+                    ],
+                    borderColor: 'rgb(75, 192, 192)',
+                    backgroundColor: 'rgb(75, 192, 192, .1)',
+                    lineTension: 0.1
+                }]
+            }
         });
     </script>
 @endsection
