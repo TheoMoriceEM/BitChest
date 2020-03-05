5079b1d7fdcc8061718b357248e9da2be21bca27
TheoMoriceEM <theo.morice5@gmail.com>
2020-03-05 20:19:05 +0100

    Add comments

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -26,9 +26,9 @@ class WalletController extends Controller
             'base_uri' => 'https://min-api.cryptocompare.com'
         ]);
 
-        $this->conversion_currency = config('currency')['api_id'];
+        $this->conversion_currency = config('currency')['api_id']; // Set real currency for conversion
 
-        $this->api_ids = [];
+        $this->api_ids = []; // Init an array for currencies' API IDs (for the API call)
     }
 
     /**
@@ -36,37 +36,39 @@ class WalletController extends Controller
      */
     public function index()
     {
-        $user = User::find(Auth::id());
+        $user = User::find(Auth::id()); // Get logged in user
 
         $currencies = $user
-            ->transactions
-            ->where('sold', 0)
-            ->groupBy('currency_id')
+            ->transactions // Get user's transactions
+            ->where('sold', 0) // Unsold
+            ->groupBy('currency_id') // Group them by currency
             ->map(function ($row) {
-                    $this->api_ids[] = $row->first()->currency->api_id;
+                    $this->api_ids[] = $row->first()->currency->api_id; // Fill API IDs array for future API call
                     return [
                         'currency'          => $row->first()->currency,
-                        'total_quantity'    => $row->sum('quantity'),
-                        'total_amount'      => $row->sum('amount')
+                        'total_quantity'    => $row->sum('quantity'), // Calculate the total of this currency
+                        'total_amount'      => $row->sum('amount') // Calculate the amount the user spent
                     ];
                 });
 
+        // API request to get data on several cryptocurrencies
         $response = $this->client->get('data/pricemultifull', [
             'query' => [
-                'fsyms' => implode(',', $this->api_ids),
-                'tsyms' => $this->conversion_currency
+                'fsyms' => implode(',', $this->api_ids), // Cryptocurrencies to get data of
+                'tsyms' => $this->conversion_currency // Real currency for conversion
             ]
         ]);
 
-        $this->data = json_decode($response->getBody())->RAW;
+        $this->data = json_decode($response->getBody())->RAW; // Get data from JSON
 
+        // Loop through currencies to add data from API
         $currencies = $currencies->map(function ($currency) {
             $conversion_currency = $this->conversion_currency;
             $api_id = $currency['currency']->api_id;
 
             $currency['current_rate'] = $this->data->$api_id->$conversion_currency->PRICE;
             $currency['change'] = $this->data->$api_id->$conversion_currency->CHANGE24HOUR > 0 ? '+' : '-';
-            $currency['increase'] = $currency['current_rate'] * $currency['total_quantity'] - $currency['total_amount'];
+            $currency['increase'] = $currency['current_rate'] * $currency['total_quantity'] - $currency['total_amount']; // Compare what the user spent to what they could get if they sold it all to calculate increase/decrease
 
             return $currency;
         });
