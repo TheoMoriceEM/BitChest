a8895a61dc3da93717cb7be0724645ec930aea64
TheoMoriceEM <theo.morice5@gmail.com>
2020-03-07 23:09:34 +0100

    Create middlewares to protect non-authorized parts of the app

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::middleware('auth')->group(function () {
 
     Route::prefix('currencies')->name('currencies.')->group(function () {
         Route::get('/', 'CurrencyController@index')->name('index');
-        Route::get('/{currency}', 'CurrencyController@show')->name('show');
+        Route::middleware('client')->get('/{currency}', 'CurrencyController@show')->name('show');
     });
 
-    Route::prefix('transactions')->name('transactions.')->group(function () {
+    Route::middleware('client')->prefix('transactions')->name('transactions.')->group(function () {
         Route::get('/create/{currency}', 'TransactionController@create')->name('create');
         Route::post('/', 'TransactionController@store')->name('store');
         Route::get('/{currency?}', 'TransactionController@index')->name('index');
@@ -29,17 +29,20 @@ Route::middleware('auth')->group(function () {
     });
 
     Route::prefix('users')->name('users.')->group(function () {
-        Route::get('/', 'UserController@index')->name('index');
-        Route::get('/create', 'UserController@create')->name('create');
-        Route::get('/{user}/edit', 'UserController@edit')->name('edit');
+        Route::middleware('admin')->group(function () {
+            Route::get('/', 'UserController@index')->name('index');
+            Route::get('/create', 'UserController@create')->name('create');
+            Route::get('/{user}/edit', 'UserController@edit')->name('edit');
+            Route::post('/', 'UserController@store')->name('store');
+            Route::patch('/{user}', 'UserController@update')->name('update');
+            Route::delete('/{user}', 'UserController@destroy')->name('destroy');
+        });
+
         Route::get('/my-account', 'UserController@editMyAccount')->name('editMyAccount');
-        Route::post('/', 'UserController@store')->name('store');
-        Route::patch('/{user}', 'UserController@update')->name('update');
         Route::patch('/my-account/{user}', 'UserController@updateMyAccount')->name('updateMyAccount');
-        Route::delete('/{user}', 'UserController@destroy')->name('destroy');
     });
 
-    Route::get('/wallet', 'WalletController@index')->name('wallet');
+    Route::middleware('client')->get('/wallet', 'WalletController@index')->name('wallet');
 
     Route::get('apiGetPrice/{fsym}', function ($fsym) {
         $api = new App\API;
