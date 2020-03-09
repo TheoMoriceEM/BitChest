c7ebb9a7d47f6750eae459a1bda3eee154b9fd6e
TheoMoriceEM <theo.morice5@gmail.com>
2020-03-09 18:00:39 +0100

    Apply balance middleware only on client parts

--- app/Http/Middleware/CalculateBalance.php
+++ app/Http/Middleware/CalculateBalance.php
@@ -29,38 +29,40 @@ class CalculateBalance
      */
     public function handle($request, Closure $next)
     {
-        $api = new API;
+        if (Auth::user()->status == 'admin') {
+            $api = new API;
 
-        $currencies = Auth::user()
-            ->transactions // Get user's transactions
-            ->where('sold', 0) // Unsold
-            ->groupBy('currency_id') // Group them by currency
-            ->map(function ($row) {
-                    $this->api_ids[] = $row->first()->currency->api_id; // Fill API IDs array for future API call
-                    return [
-                        'currency' => $row->first()->currency,
-                        'quantity' => $row->sum('quantity') // Calculate the total of this currency
-                    ];
-                });
+            $currencies = Auth::user()
+                ->transactions // Get user's transactions
+                ->where('sold', 0) // Unsold
+                ->groupBy('currency_id') // Group them by currency
+                ->map(function ($row) {
+                        $this->api_ids[] = $row->first()->currency->api_id; // Fill API IDs array for future API call
+                        return [
+                            'currency' => $row->first()->currency,
+                            'quantity' => $row->sum('quantity') // Calculate the total of this currency
+                        ];
+                    });
 
-        if ($currencies->isNotEmpty()) {
-            $this->data = $api->getMultipleData(implode(',', $this->api_ids)); // Get data from the API
+            if ($currencies->isNotEmpty()) {
+                $this->data = $api->getMultipleData(implode(',', $this->api_ids)); // Get data from the API
+            }
+
+            // Loop through currencies to add data from API
+            $currencies = $currencies->map(function ($currency) {
+                $api_id = $currency['currency']->api_id;
+                $currency['current_rate'] = $this->data[$api_id]['current_rate'];
+                return $currency;
+            });
+
+            $balance = 0;
+
+            foreach ($currencies as $currency) {
+                $balance += $currency['quantity'] * $currency['current_rate'];
+            }
+
+            session(['balance' => round($balance, 2)]);
         }
-
-        // Loop through currencies to add data from API
-        $currencies = $currencies->map(function ($currency) {
-            $api_id = $currency['currency']->api_id;
-            $currency['current_rate'] = $this->data[$api_id]['current_rate'];
-            return $currency;
-        });
-
-        $balance = 0;
-
-        foreach ($currencies as $currency) {
-            $balance += $currency['quantity'] * $currency['current_rate'];
-        }
-
-        session(['balance' => round($balance, 2)]);
 
         return $next($request);
     }
--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,33 @@
 |
 */
 
-Route::middleware('auth', 'balance')->group(function () {
-    Route::get('/', 'CurrencyController@index')->name('home');
-    Route::get('/home', 'CurrencyController@index');
+Route::middleware('auth')->group(function () {
+    Route::middleware('balance')->group(function () {
+        Route::get('/', 'CurrencyController@index')->name('home');
+        Route::get('/home', 'CurrencyController@index');
 
-    Route::prefix('currencies')->name('currencies.')->group(function () {
-        Route::get('/', 'CurrencyController@index')->name('index');
-        Route::middleware('client')->get('/{currency}', 'CurrencyController@show')->name('show');
-    });
+        Route::prefix('currencies')->name('currencies.')->group(function () {
+            Route::get('/', 'CurrencyController@index')->name('index');
+            Route::middleware('client')->get('/{currency}', 'CurrencyController@show')->name('show');
+        });
 
-    Route::middleware('client')->prefix('transactions')->name('transactions.')->group(function () {
-        Route::get('/{currency?}', 'TransactionController@index')->name('index');
-        Route::get('/create/{currency}', 'TransactionController@create')->name('create');
-        Route::post('/', 'TransactionController@store')->name('store');
-        Route::patch('/{currency}/{transaction?}', 'TransactionController@update')->name('update');
-        Route::get('/sell/{currency}', 'TransactionController@sell')->name('sell');
+        Route::middleware('client')->prefix('transactions')->name('transactions.')->group(function () {
+            Route::get('/{currency?}', 'TransactionController@index')->name('index');
+            Route::get('/create/{currency}', 'TransactionController@create')->name('create');
+            Route::post('/', 'TransactionController@store')->name('store');
+            Route::patch('/{currency}/{transaction?}', 'TransactionController@update')->name('update');
+            Route::get('/sell/{currency}', 'TransactionController@sell')->name('sell');
+        });
+
+        Route::prefix('account')->name('account.')->group(function () {
+            Route::get('/edit', 'UserController@editAccount')->name('edit');
+            Route::patch('/{user}', 'UserController@updateAccount')->name('update');
+        });
+
+        Route::middleware('client')->get('/wallet', 'WalletController@index')->name('wallet');
     });
 
     Route::middleware('admin')->resource('users', 'UserController')->except(['show']);
-
-    Route::prefix('account')->name('account.')->group(function () {
-        Route::get('/edit', 'UserController@editAccount')->name('edit');
-        Route::patch('/{user}', 'UserController@updateAccount')->name('update');
-    });
-
-    Route::middleware('client')->get('/wallet', 'WalletController@index')->name('wallet');
 });
 
 Route::prefix('api')->name('api.')->group(function () {
